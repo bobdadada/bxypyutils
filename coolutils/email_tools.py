86dd93ba629cf31f382cc436ba8fbf62f9164823
baoxingyu <baoxingyubob@outlook.com>
2021-09-17 22:31:27 +0800

    完善email_tools使其可以SSL

--- coolutils/email_tools.py
+++ coolutils/email_tools.py
@@ -13,24 +13,29 @@ def notify_self(server, emailbox, msg, subject=None):
     """
     Send an email to ourself
 
-    :param server: (host, port) gives the hostname and port of smtp service.
+    :param server: (host, port, needssl) gives the hostname and port of smtp service, and will build a 
+        TLS-encrypted connection if needssl is True.
     :param emailbox: (addr, passwd) gives the address and password of one's mailbox.
     :param msg: message.
     :param subject: subject of the message. default: None.
 
     >>>notify_self(('localhost', 25), ('name@domain', 'passwd'), 'A test email', 'Test')
+    >>>notify_self(('localhost', 465, True), ('name@domain', 'passwd'), 'A test email', 'Test')
     """
     if len(emailbox) != 2:
         raise ValueError('Please enter the correct email address and password with format (addr, passwd)')
     addr, password = emailbox
 
     if isinstance(server, six.string_types):
-        host, port = server, 25
+        host, port, needssl = server, 25, False
     elif isinstance(server, (tuple, list)):
         if len(server) == 1:
-            host, port = server[0], 25
+            host, port, needssl = server[0], 25, False
         elif len(server) == 2:
             host, port = server
+            needssl = False
+        elif len(server) == 3:
+            host, port, needssl = server
         else:
             raise ValueError('Please enter the host and port of the SMTP service with format (host, port)')
     else:
@@ -42,7 +47,12 @@ def notify_self(server, emailbox, msg, subject=None):
     message['To'] =  Header(formataddr(('Me', addr)), 'utf-8')    # Receiver
     message['Subject'] = Header(str(subject), 'utf-8')
 
-    with smtplib.SMTP() as smtpObj:
-        smtpObj.connect(host, port)
-        smtpObj.login(addr, password)
-        smtpObj.sendmail(addr, [addr], message.as_string())
+    if needssl:
+        smtpObj = smtplib.SMTP_SSL(host, port)
+    else:
+        smtpObj = smtplib.SMTP(host, port)
+    
+    smtpObj.login(addr, password)
+    smtpObj.sendmail(addr, [addr], message.as_string())
+
+    smtpObj.close()
