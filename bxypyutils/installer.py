189112c5ee6c08191ff5adab4fac81031b7438bd
baoxingyu <baoxingyubob@outlook.com>
2021-10-01 19:58:19 +0800

    重新对版本进行编号，并修复installer.py中的bug

--- bxypyutils/installer.py
+++ bxypyutils/installer.py
@@ -19,20 +19,22 @@ def install_all(srcdir, objdir, quiet=False, exception_ok=False):
 
     >>>install('dist/', 'bin/')
     """
-    except_filenames = []
-    for filename in os.listdir(srcdir):
+    failed_softwares = []
+    for name in os.listdir(srcdir):
         try:
-            install(os.path.join(srcdir, filename),
-                    objdir, quiet, exception_ok)
-        except:
-            except_filenames.append(filename)
-    if except_filenames:
+            install(os.path.join(srcdir, name),
+                    objdir, quiet, exception_ok=False)
+        except Exception as e:
+            if not quiet:
+                print(e)
+            failed_softwares.append(name)
+    if failed_softwares:
         if not quiet:
             print("[!]Failed to update software %s" %
-                  (','.join(except_filenames)))
+                  (','.join(failed_softwares)))
         if not exception_ok:
             raise Exception("[!]Failed to update software %s" %
-                            (','.join(except_filenames)))
+                            (','.join(failed_softwares)))
 
 
 def install(src, objdir, quiet=False, exception_ok=False):
@@ -55,61 +57,101 @@ def install(src, objdir, quiet=False, exception_ok=False):
             raise Exception("[!]%s doesn't exist" % src)
 
 
-def install_dir(src, objdir, quiet=False, exception_ok=False):
+def install_dir(srcdir, objdir, quiet=False, exception_ok=False):
     """
     Update or install a directory to a specific directory by comparing timestamps.
 
-    :param src: the path of the directory.
+    :param srcdir: the path of the directory.
     :param objdir: the directory where the directory is installed.
     :param quiet: flag whether to print the installation info quietly. default: False.
     :param exception_ok: flag whether to ignore the raised exception. default: False.
 
     >>>install('test/', 'bin/')
     """
-    src = os.path.abspath(src)
-    if not os.path.isdir(src):
+    srcdir = os.path.abspath(srcdir)
+    objdir = os.path.abspath(objdir)
+
+    if not os.path.isdir(srcdir):
         if not exception_ok:
-            raise Exception('[!]%s is not a directory' % src)
+            raise Exception('[!]%s is not a directory' % srcdir)
         return
 
-    _, dirname = os.path.split(src)
+    if srcdir == objdir:
+        if not exception_ok:
+            raise Exception(
+                '[!]The source path %s conflicts with the destination path' % srcdir)
+        return
 
-    objdir = os.path.join(objdir, dirname)
-    os.makedirs(objdir, exist_ok=True)
+    topath = os.path.join(objdir, os.path.split(srcdir)[1])
+    os.makedirs(topath, exist_ok=True)
 
-    for name in os.listdir(src):
-        path = os.path.join(src, name)
-        if os.path.isfile(path):
-            install_file(path, objdir, quiet=quiet, exception_ok=exception_ok)
-        else:
-            install_dir(path, objdir, quiet=quiet, exception_ok=exception_ok)
+    if not quiet:
+        print("[+]Updating %s" % srcdir)
+
+    failed_filenames = []
+
+    for dirpath, dirnames, filenames in os.walk(srcdir):
+
+        for filename in filenames:
+            path = os.path.join(dirpath, filename)
+            try:
+                install_file(path, dirpath.replace(srcdir, topath),
+                             quiet=True, exception_ok=False)
+            except:
+                failed_filenames.append(path)
+
+        # ingore the destination diretory to prevent endless recursion
+        remove_dirname = None
+        for dirname in dirnames:
+            path = os.path.join(dirpath, dirname)
+            if path == objdir:
+                remove_dirname = dirname
+                break
+        if remove_dirname:
+            dirnames.remove(remove_dirname)
+
+    if failed_filenames:
+        if not quiet:
+            print("[!]For %s, fail to update files %s" %
+                  (srcdir, ',\n\t'.join(failed_filenames)))
+
+        if not exception_ok:
+            raise Exception("[!]For %s, failed to update files %s" %
+                            (srcdir, ',\n\t'.join(failed_filenames)))
+    else:
+        if not quiet:
+            print("[-]Successfully update %s" % srcdir)
 
 
-def install_file(src, objdir, quiet=False, exception_ok=False):
+def install_file(srcfile, objdir, quiet=False, exception_ok=False):
     """
     Update or install a file to a specific directory by comparing timestamps.
 
-    :param src: the path of the file.
+    :param srcfile: the path of the file.
     :param objdir: the directory where the file is installed.
     :param quiet: flag whether to print the installation info quietly. default: False.
     :param exception_ok: flag whether to ignore the raised exception. default: False.
 
     >>>install('test.exe', 'test.txt')
     """
-    src = os.path.abspath(src)
-    if not os.path.isfile(src):
+    srcfile = os.path.abspath(srcfile)
+    objdir = os.path.abspath(objdir)
+
+    if not os.path.isfile(srcfile):
         if not exception_ok:
-            raise Exception('[!]%s is not a file' % src)
+            raise Exception('[!]%s is not a file' % srcfile)
         return
 
-    _, filename = os.path.split(src)
+    os.makedirs(objdir, exist_ok=True)
+
+    _, filename = os.path.split(srcfile)
 
     update_flag = False
 
     if filename in os.listdir(objdir):
         # 时间戳比较
         pst = os.stat(os.path.join(objdir, filename))
-        cst = os.stat(src)
+        cst = os.stat(srcfile)
         if pst.st_mtime < cst.st_mtime:
             update_flag = True
     else:
@@ -117,17 +159,18 @@ def install_file(src, objdir, quiet=False, exception_ok=False):
 
     if update_flag:
         if not quiet:
-            print("[+]Updating %s" % src)
+            print("[+]Updating %s" % srcfile)
 
         try:
-            shutil.copy(src, objdir)
+            shutil.copy(srcfile, objdir)
         except:
             if not quiet:
-                print("[!]Failed to update %s" % src)
+                print("[!]Fail to update %s" % srcfile)
             if not exception_ok:
                 raise
 
         if not quiet:
-            print("[-]Successfully update %s" % src)
+            print("[-]Successfully update %s" % srcfile)
     else:
-        print("[*]Software %s does not need to be updated" % src)
+        if not quiet:
+            print("[*]Software %s does not need to be updated" % srcfile)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bxypyutils',
-    version='1.2',
+    version='1.0.3',
     packages=['bxypyutils'],
     install_requires=['six'],
     url='https://github.com/bobdadada/bxypyutils',
